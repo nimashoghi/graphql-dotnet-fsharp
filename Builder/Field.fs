7ca88511858ad75e5a0c97ca6ee646be5186640c
Nima Shoghi <nimashoghi@gmail.com>
2019-01-13 17:26:36 -0500

    Added typed fields Added field type inference -> fixes runtime errors Fixed vscode debug config

--- Builder/Field.fs
+++ Builder/Field.fs
@@ -2,37 +2,114 @@
 module GraphQL.FSharp.Builder.Field
 
 open System
+open FSharp.Quotations
+open FSharp.Linq.RuntimeHelpers
 open GraphQL.Types
+open GraphQL.Subscription
 open GraphQL.Resolvers
 
 let inline private set f (x: EventStreamFieldType) = f x; x
 
-type FieldBuilder() =
+[<AutoOpen>]
+module private TypeInference =
+    open System.Collections.Generic
+    open GraphQL.Utilities
+
+    let (|Nullable|_|) (``type``: Type) =
+        if ``type``.IsGenericType && ``type``.GetGenericTypeDefinition() = typedefof<Nullable<_>>
+        then Some``type``.GenericTypeArguments.[0]
+        else None
+
+    let (|ArrayType|_|) (``type``: Type) =
+        if ``type``.IsArray
+        then Some (``type``.GetElementType ())
+        else None
+
+    let (|EnumerableType|_|) (``type``: Type) =
+        ``type``
+            .GetInterfaces()
+            |> Array.tryFind (fun ``interface`` ->
+                ``interface``.IsGenericType &&
+                ``interface``.GetGenericTypeDefinition () = typedefof<IEnumerable<_>>)
+            |> Option.map (fun ``interface`` -> ``interface``.GenericTypeArguments.[0])
+
+    let (|Enumerable|_|) (``type``: Type) =
+        match ``type`` with
+        | ArrayType underlyingType
+        | EnumerableType underlyingType -> Some underlyingType
+        | _ -> None
+
+    // TODO: Check nullable stuff
+    let rec infer (``type``: Type) =
+        match ``type`` with
+        | Nullable underlyingType -> infer underlyingType
+        | Enumerable underlyingType -> ListGraphType (infer underlyingType) :> IGraphType
+        // TODO: clean up
+        | underlyingType -> GraphTypeInstanceRegistry().Get underlyingType
+
+    let getReturnType (resolver: IFieldResolver) =
+        resolver
+            .GetType()
+            .GetInterfaces()
+            |> Array.tryFind (fun ``interface`` ->
+                ``interface``.IsGenericType &&
+                ``interface``.GetGenericTypeDefinition() = typedefof<IFieldResolver<_>>)
+            |> Option.map (fun ``interface`` -> ``interface``.GenericTypeArguments.[0])
+
+    let (|TypedResolver|_|) resolver =
+        if resolver = null
+        then None
+        else getReturnType resolver
+
+    let shouldInferField (field: FieldType) = field.Type = null && field.ResolvedType = null
+
+    let inferField (field: FieldType) =
+        match field.Resolver with
+        | TypedResolver retn ->
+            field.ResolvedType <- infer retn
+            field
+        | _ -> field
+
+type FieldBuilder<'source>() =
     inherit BuilderMetadataBase<EventStreamFieldType>()
 
     [<CustomOperation "type">]
-    member __.Type (field, ``type``) = set (fun x -> x.Type <- ``type``) field
-    member __.Type (field, ``type``) = set (fun x -> x.ResolvedType <- ``type``) field
+    member __.Type (field, ``type``) =
+        set (fun x -> x.Type <- ``type``) field
+    member __.Type (field, ``type``) =
+        set (fun x -> x.ResolvedType <- ``type``) field
 
     [<CustomOperation "defaultValue">]
-    member __.DefaultValue (field, ``default``) = set (fun x -> x.DefaultValue <- ``default``) field
+    member __.DefaultValue (field, ``default``) =
+        set (fun x -> x.DefaultValue <- ``default``) field
 
     [<CustomOperation "arguments">]
-    member __.Arguments (field, arguments: _ list) = set (fun x -> x.Arguments <- QueryArguments arguments) field
+    member __.Arguments (field, arguments: _ list) =
+        set (fun x -> x.Arguments <- QueryArguments arguments) field
+
+    [<CustomOperation "get">]
+    member __.Get (field, [<ReflectedDefinition>] getter: Expr<'source -> 'property>) =
+        let resolver = ExpressionFieldResolver (LeafExpressionConverter.QuotationToLambdaExpression <@ Func<_, _> %getter @>)
+        set (fun x -> x.Resolver <- resolver) field
 
     [<CustomOperation "resolve">]
-    member __.Resolve (field, resolver) =
+    member __.Resolve (field, resolver: ResolveFieldContext<'source> -> _) =
         let resolver = FuncFieldResolver<_, _> (Func<_, _> resolver)
         set (fun x -> x.Resolver <- resolver) field
 
     [<CustomOperation "subscribe">]
-    member __.Subscribe (field, subscribe) =
+    member __.Subscribe (field, subscribe: ResolveEventStreamContext<'source> -> _) =
         let subscriber = EventStreamResolver<_, _> (Func<_, _> subscribe)
         set (fun x -> x.Subscriber <- subscriber) field
 
     [<CustomOperation "subscribeAsync">]
-    member __.SubscribeAsync (field, subscribeAsync) =
+    member __.SubscribeAsync (field, subscribeAsync: ResolveEventStreamContext<'source> -> _) =
         let asyncSubscriber = AsyncEventStreamResolver<_, _> (Func<_, _> subscribeAsync)
         set (fun x -> x.AsyncSubscriber <- asyncSubscriber) field
 
-let field = FieldBuilder ()
+    member __.Run (field: FieldType) =
+        if shouldInferField field
+        then inferField field
+        else field
+
+let field<'source> = FieldBuilder<'source> ()
--- GraphQL.FSharp.TestServer/Startup.fs
+++ GraphQL.FSharp.TestServer/Startup.fs
@@ -7,42 +7,42 @@ open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
 
 open GraphQL
+open GraphQL.Types
 open GraphQL.Server
 open GraphQL.Server.Ui.Playground
 open GraphQL.FSharp.Builder
 
 module GQL =
-    let myQuery () = query {
+    let myQuery = query {
         fields [
             field {
                 name "myQuery"
+                // ``type`` typeof<IntGraphType>
                 resolve (fun ctx -> 1)
             }
         ]
     }
 
-    let mySchema () = schema {
-        query (myQuery ())
+    let mySchema = schema {
+        query myQuery
     }
 
 type Startup() =
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     member this.ConfigureServices(services: IServiceCollection) =
-        services.AddSingleton<Types.Schema> (GQL.mySchema ()) |> ignore
+        services
+            .AddSingleton<Schema>(GQL.mySchema)
+            |> ignore
         services
             .AddGraphQL(fun options ->
                 options.ExposeExceptions <- true)
             |> ignore
         ()
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
         if env.IsDevelopment() then
             app.UseDeveloperExceptionPage() |> ignore
 
-        app.UseGraphQL<Types.Schema> "/graphql" |> ignore
-        app.UseGraphQLPlayground (GraphQLPlaygroundOptions()) |> ignore
+        app.UseGraphQL<Schema> "/graphql" |> ignore
+        GraphQLPlaygroundOptions () |> app.UseGraphQLPlayground |> ignore
 
         app.Run(fun context -> context.Response.WriteAsync("Hello World!")) |> ignore
